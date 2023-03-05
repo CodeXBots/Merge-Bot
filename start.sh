a0376669698d70d9b4c8dc65065c19783b2414f2
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-03-05 11:12:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/MovieVillaYT/Link-Search-Bot /Link-Search-Bot
+git clone https://github.com/abdulkhalik88822/Link-Search-Bot /Link-Search-Bot
 cd /Link-Search-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
