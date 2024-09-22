f46a14c7863a8bb3cdab6cbbf59e03df642dd16b
CodeXBots <RahulReviewsYT@gmail.com>
2024-09-22 17:17:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
-echo "Cloning Repo...."
-git clone https://github.com/CodeXBots/Link-Search-Bot /Link-Search-Bot
-cd /Link-Search-Bot
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 main.py
+python=venv/bin/python
+$python get_config.py && $python bot.py
