ab0fa99f229bef5c9920dbda532193092c45c996
CodeXBots <RahulReviewsYT@gmail.com>
2024-09-22 17:13:26 +0530

    Update and rename configs.py to config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Config(object):
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    TELEGRAM_API = os.environ.get("TELEGRAM_API")
+    OWNER = os.environ.get("OWNER")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "CodeXBro")
+    PASSWORD = os.environ.get("PASSWORD")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
+    IS_PREMIUM = False
+    MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
